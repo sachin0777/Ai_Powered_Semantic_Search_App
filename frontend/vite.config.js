fd6042c8800464b2723a1548c3e0430655937814
sachinux <sachin2gita@gmail.com>
2025-09-08 14:40:31 +0530

    modified config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/semantic-search/',
+  // Change this:
+  base: './',   // ⚠️ was '/semantic-search/' before
   build: {
     outDir: '../dist',
     assetsDir: 'assets',
